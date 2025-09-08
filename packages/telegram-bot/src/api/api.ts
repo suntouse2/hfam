8b5e93c292e9c7d5e3002ca3cf7508e840e06414
suntouse2 <suntouse2@gmail.com>
2025-09-08 21:09:38 +0400

    ports fix

--- packages/telegram-bot/src/api/api.ts
+++ packages/telegram-bot/src/api/api.ts
@@ -1,7 +1,7 @@
 import got from 'got'
 
 export const api = got.extend({
-	prefixUrl: 'http://localhost:3001',
+	prefixUrl: 'http://localhost:4000',
 	responseType: 'json',
 	timeout: { request: 10000 },
 	retry: { limit: 2 },
