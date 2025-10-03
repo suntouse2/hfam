c6ac7f6c6b6b73d4eb08b92709e57e0b1a3b143a
suntouse2 <suntouse2@gmail.com>
2025-10-04 00:05:08 +0400

    latest

--- packages/engine/generated/prisma/wasm.js
+++ packages/engine/generated/prisma/wasm.js
@@ -219,7 +219,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
