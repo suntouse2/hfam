89e2c4d7d41b79fe55ff3e2f6adf2e146f3915e1
suntouse2 <suntouse2@gmail.com>
2025-10-15 19:17:46 +0400

    a

--- packages/engine/src/handlers/handlePayCbLink.ts
+++ packages/engine/src/handlers/handlePayCbLink.ts
@@ -2,5 +2,5 @@ import { DOMAIN } from "@hfam/shared";
 import type { ConnectorDTO } from "@hfam/shared/dto/index";
 
 export const handlePayCbLink = (connectorId: ConnectorDTO["id"]) => {
-	return `${DOMAIN}/pay/callback/${connectorId}`;
+	return `${DOMAIN}/core/pay/callback/${connectorId}`;
 };
