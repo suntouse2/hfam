2815c82e3cfb045b45c41c510fe32ec7cf9c7abe
suntouse2 <suntouse2@gmail.com>
2025-10-25 17:20:11 +0400

    only success notifies

--- packages/engine/src/routes/pay.ts
+++ packages/engine/src/routes/pay.ts
@@ -11,42 +11,21 @@ export const pay = Router();
 pay.post("/", auth, async (req, res) => {
 	const payload = payCreateSchema.parse(req.body);
 	const payment = await handlePay(payload);
-
-	try {
-		await fetch(
-			`http://localhost:${process.env.BOT_PORT}/notify/payment/${payment.id}`,
-			{
-				method: "POST",
-				body: JSON.stringify(payment),
-				headers: {
-					"Content-Type": "application/json",
-					Authorization: `Bearer ${process.env.API_KEY}`,
-				},
-			},
-		);
-	} catch (error) {
-		console.error(error);
-	}
 	res.json(payment);
 });
 
 pay.post("/callback/:connectorId", async (req, res) => {
-	console.log("прилетел лог коллбека 2");
 	const { token } = req.query;
 
 	if (token !== process.env.API_KEY) {
-		console.log("no passed token in query");
 		throw ErrorAPI.badRequest("No passed token in query");
 	}
 
-	const connectorId = z.coerce
-		.number()
-		.nonnegative()
-		.parse(req.params.connectorId);
-	console.log("спарсился2");
+	//biome-ignore format: no need
+	const connectorId = z.coerce.number().nonnegative().parse(req.params.connectorId);
+
 	const payment = await handleCb(req, connectorId);
-	console.log("cb обработался");
-	console.log("payment", payment);
+
 	if (payment) {
 		await fetch(
 			`http://localhost:${process.env.BOT_PORT}/notify/payment/${payment.id}`,
